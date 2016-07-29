afbcd31d75151399d15293c71abee2f995a367ac
Miro Nieminen <miro.nieminen@futurice.com>
2016-07-29 17:06:20 +0300

    [WIP] Skeleton of index.js
    NOTE: currently they require mankov from relative path!

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const Mankov = require('../../mankov');
+
+const IltaaCommander = require('./iltaa-commander');
+const MoroResponder = require('./moro-responder');
+const LogMonitor = require('./log-monitor');
+
+const TG_TOKEN = process.env.MANKOV_TG_TOKEN;
+const WEBHOOK_URL = process.env.MANKOV_WEBHOOK_URL;
+
+
+// Create Mankov instance
+const mankov = new Mankov();
+
+// Attach Monitor, Commander, & Responder
+mankov.addCommander(new IltaaCommander());
+mankov.addResponder(new MoroResponder(100, 'juuh elikäs'));
+mankov.addMonitor(new LogMonitor());
+
+// Create Telegram-bot to that instance
+mankov.createBot('telegram', 'MankovBot', { token: TG_TOKEN })
+  .then(bot => {
+    bot.client.setWebHook(WEBHOOK_URL);
+  });
+
