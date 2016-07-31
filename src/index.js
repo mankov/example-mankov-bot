2869f86ba8b929ee280298d06405eae0444b3421
Miro Nieminen <miro.nieminen@futurice.com>
2016-07-31 16:00:43 +0300

    Housecleaning - fix linting errors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+const _ = require('lodash');
 const Mankov = require('../../mankov');
 
 const IltaaCommander = require('./iltaa-commander');
 const MoroResponder = require('./moro-responder');
 const LogMonitor = require('./log-monitor');
 
+
 const TG_TOKEN = process.env.MANKOV_TG_TOKEN;
 const WEBHOOK_URL = process.env.MANKOV_WEBHOOK_URL;
 
-const usePolling = (!WEBHOOK_URL) ? true : false;
+const usePolling = !_.isUndefined(WEBHOOK_URL);
 
 // Create Mankov instance
 const mankov = new Mankov();
@@ -18,7 +20,9 @@ mankov.addResponder(new MoroResponder(100, 'juuh elikäs'));
 mankov.addMonitor(new LogMonitor());
 
 // Create Telegram-bot to that instance
-mankov.createBot('telegram', 'MankovBot', { token: TG_TOKEN, optional: { webHook: !usePolling, polling: usePolling }})
+mankov.createBot('telegram', 'MankovBot', {
+  token: TG_TOKEN, optional: { webHook: !usePolling, polling: usePolling }
+})
 .then(bot => {
   if (!usePolling) bot.client.setWebHook(WEBHOOK_URL);
 });
